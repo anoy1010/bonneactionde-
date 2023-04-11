9bbec9e29ab08eae910c025db39f7319900c5000
CHEICK <cheickdev1010@gmail.com>
2023-04-11 17:24:00 +0000

    Dashbord.php

---
+++ Dashbord.php
@@ -0,0 +1,43 @@
+<?php include('../include/head.php') ?>
+<?php include('../include/db.php') ?>
+
+<body>
+    <div class="overflow-x-auto">
+        <table class="table table-compact w-full">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Date de naissance</th>
+                    <th>Ville</th>
+                    <th>Pays</th>
+                    <th>Address mail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <td>Cy Ganderton</td>
+                    <td>Quality Control Specialist</td>
+                    <td>Littel, Schaden and Vandervort</td>
+                    <td>Canada</td>
+                    <td>12/16/2020</td>
+                    <td>Blue</td>
+                </tr>
+
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>id</th>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Date de naissance</th>
+                    <th>Ville</th>
+                    <th>Pays</th>
+                    <th>Address mail</th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+</body>
